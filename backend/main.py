55f6a74cf88b9545ff32c721c375bb0b04909153
joelajs007-source <joelajs007@gmail.com>
2026-03-09 22:52:57 +0530

    fix supabase ssl connection

--- backend/main.py
+++ backend/main.py
@@ -60,11 +60,11 @@ twilio_client = Client(TWILIO_SID, TWILIO_AUTH)
 # DATABASE CONNECTION
 # ======================================================
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 def get_db():
-    conn = psycopg2.connect(
-        DATABASE_URL,
-        sslmode="require"
-    )
+    conn = psycopg2.connect(DATABASE_URL)
     return conn
 
 
